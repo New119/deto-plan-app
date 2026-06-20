52654e89991da600f649d2d32a8b203dfabffe62
Ryota <ryota07119@icloud.com>
2026-06-20 09:54:21 +0900

    update url to ai-date-maker and title

--- backend/backend.py
+++ backend/backend.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://deto-plan-app.vercel.app",],
+    allow_origins=["http://localhost:5173","https://ai-date-maker.vercel.app","https://deto-plan-app.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
